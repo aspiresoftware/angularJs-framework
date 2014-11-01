80f22d9194dde7a396b877415a72b0099b17f932
Pratik Patel <pratik.patel@aspiresoftware.in>
2014-11-01 16:58:00 +0530

    changes some fixes

--- Gruntfile.js
+++ Gruntfile.js
@@ -73,7 +73,7 @@ module.exports = function (grunt) {
       options: {
         port: 9999,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: '192.168.1.13',
+        hostname: '0.0.0.0',
         livereload: 35729
       },
       livereload: {
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-angularJs-framework
-===================
+FINTECH-UI
+==========
+
+FINTECH-UI
